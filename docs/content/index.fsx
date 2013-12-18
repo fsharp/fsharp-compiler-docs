647b180657d5a39eca1ad5bdf068ddb823876e6f
Tomas Petricek <tomas@tomasp.net>
2013-12-18 10:19:48 +0200

    Add FAKE build, generate version numbers

---
+++ docs/content/index.fsx
@@ -0,0 +1,31 @@
+(**
+F# Compiler Services
+====================
+
+The F# compiler services package contains a custom build of the F# compiler that
+exposes additional functionality for implementing F# language bindings, additional
+tools based on the compiler or refactoring tools. The package also includes F# 
+interactive service that can be used for embedding F# scripting into your applications."""
+
+<div class="row">
+  <div class="span1"></div>
+  <div class="span6">
+    <div class="well well-small" id="nuget">
+      The F# Compiler Services package can be <a href="https://nuget.org/packages/FSharp.Compiler.Services">installed from NuGet</a>:
+      <pre>PM> Install-Package FSharp.Compiler.Services</pre>
+    </div>
+  </div>
+  <div class="span1"></div>
+</div>
+
+More content
+------------
+
+Goes here
+ 
+Contributing and copyright
+--------------------------
+
+Goes here
+
+*)

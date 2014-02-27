ac354429791457a8d53d2b73438d3707a155cb35
Cameron Taggart <cameron.taggart@gmail.com>
2014-02-26 23:58:04 -0600

    #1 #2 AppVeyor SourceLink

--- build.cmd
+++ build.cmd
@@ -2,5 +2,7 @@
 if not exist packages\FAKE\tools\Fake.exe ( 
   .nuget\NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
 )
+if not exist packages\SourceLink.Fake\tools\SourceLink.fsx ( 
+  .nuget\NuGet.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
+)
 packages\FAKE\tools\FAKE.exe build.fsx %*
-pause

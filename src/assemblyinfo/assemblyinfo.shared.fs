6540cad0178f38a9cf452dcaadd0afc68647ed2a
Quentin Lord <quasilord1@gmail.com>
2014-06-25 18:45:44 +0100

    updates after merge

--- src/assemblyinfo/assemblyinfo.shared.fs
+++ src/assemblyinfo/assemblyinfo.shared.fs
@@ -1,9 +1,9 @@
 ﻿namespace System
 open System.Reflection
 
-[<assembly: AssemblyVersionAttribute("0.0.55")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.55")>]
+[<assembly: AssemblyVersionAttribute("0.0.57")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.57")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.55"
+    let [<Literal>] Version = "0.0.57"

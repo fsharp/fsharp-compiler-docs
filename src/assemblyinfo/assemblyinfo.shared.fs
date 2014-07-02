c874f18e357907fe90bee4346d9179435e7cd0b4
Quentin Lord <quasilord1@gmail.com>
2014-07-02 14:23:02 +0100

    Update version and fix VS build

--- src/assemblyinfo/assemblyinfo.shared.fs
+++ src/assemblyinfo/assemblyinfo.shared.fs
@@ -1,9 +1,9 @@
 ﻿namespace System
 open System.Reflection
 
-[<assembly: AssemblyVersionAttribute("0.0.57")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.57")>]
+[<assembly: AssemblyVersionAttribute("0.0.58")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.58")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.57"
+    let [<Literal>] Version = "0.0.58"

f581811719c3aa2288e358fea6eeb743bc719f7b
F# Software Foundation <fsgit@fastmail.fm>
2014-09-11 14:22:18 +0100

    don't reinstall FAKE every build

--- build.cmd
+++ build.cmd
@@ -1,6 +1,8 @@
 @echo off
 
-".nuget/NuGet.exe" "install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion" 
-".nuget/NuGet.exe" "install" "FSharp.Formatting" "-OutputDirectory" "packages" "-ExcludeVersion"
-".nuget/NuGet.exe" "install" "SourceLink.Fake" "-OutputDirectory" "packages" "-ExcludeVersion"
+if not exist packages\FAKE\tools\Fake.exe ( 
+  ".nuget/NuGet.exe" "install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion" 
+  ".nuget/NuGet.exe" "install" "FSharp.Formatting" "-OutputDirectory" "packages" "-ExcludeVersion"
+  ".nuget/NuGet.exe" "install" "SourceLink.Fake" "-OutputDirectory" "packages" "-ExcludeVersion"
+)
 packages\FAKE\tools\FAKE.exe build.fsx %*

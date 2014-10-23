02d1eb6e94585bda782357ff3a3ef7d6267b737f
dungpa <phananhdung309@yahoo.com>
2014-10-23 12:02:24 +0200

    Add more tests for signature symbols
    Close #231.

--- tests/service/ProjectAnalysisTests.fs
+++ tests/service/ProjectAnalysisTests.fs
@@ -4124,180 +4124,74 @@ let ``Test project31 Format C# method attributes`` () =
         |> shouldEqual 
               (set ["[<CLSCompliantAttribute (false)>]";
                     "[<Security.SecuritySafeCriticalAttribute ()>]"])
-#if FX_ATLEAST_45
 
-let checkOption (opts:string[]) s = 
-    let found = "Found '"+s+"'"
-    (if opts |> Array.exists (fun o -> o.EndsWith(s)) then found else "Failed to find '"+s+"'")
-       |> shouldEqual found
+module Project32 = 
+    open System.IO
 
-let checkOptionNotPresent (opts:string[]) s = 
-    let found = "Found '"+s+"'"
-    let notFound = "Did not expect to find '"+s+"'"
-    (if opts |> Array.exists (fun o -> o.EndsWith(s)) then found else notFound)
-       |> shouldEqual notFound
+    let fileName1 = Path.ChangeExtension(Path.GetTempFileName(), ".fs")
+    let sigFileName1 = Path.ChangeExtension(fileName1, ".fsi")
+    let base2 = Path.GetTempFileName()
+    let dllName = Path.ChangeExtension(base2, ".dll")
+    let projFileName = Path.ChangeExtension(base2, ".fsproj")
+    let fileSource1 = """
+module Sample
+let func x = x + 1
+    """
+    File.WriteAllText(fileName1, fileSource1)
 
-[<Test>]
-let ``Project file parsing example 1 Default Configuration`` () = 
-  // BUG - see https://github.com/fsharp/FSharp.Compiler.Service/issues/237
-//  if not runningOnMono then 
+    let sigFileSource1 = """
+module Sample
 
-    let projectFile = __SOURCE_DIRECTORY__ + @"/FSharp.Compiler.Service.Tests.fsproj"
-    let options = checker.GetProjectOptionsFromProjectFile(projectFile)
+val func : int -> int
+    """
+    File.WriteAllText(sigFileName1, sigFileSource1)
+    let cleanFileName a = if a = fileName1 then "file1" elif a = sigFileName1 then "sig1"  else "??"
 
-    printfn "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv" 
-    printfn "PROJ FILE %s" projectFile
-    printfn "%A" options.OtherOptions
-    printfn "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" 
+    let fileNames = [sigFileName1; fileName1]
+    let args = mkProjectCommandLineArgs (dllName, fileNames)
+    let options =  checker.GetProjectOptionsFromCommandLineArgs (projFileName, args)
 
-    checkOption options.OtherOptions "FSharp.Compiler.Service.dll"
-    checkOption options.OtherOptions "FileSystemTests.fs"
-    checkOption options.OtherOptions "--define:TRACE"
-    checkOption options.OtherOptions "--define:DEBUG"
-    checkOption options.OtherOptions "--flaterrors"
-    checkOption options.OtherOptions "--simpleresolution"
-    checkOption options.OtherOptions "--noframework"
 
 [<Test>]
-let ``Project file parsing example 1 Release Configuration`` () = 
-  // BUG - see https://github.com/fsharp/FSharp.Compiler.Service/issues/237
-//  if not runningOnMono then 
-    let projectFile = __SOURCE_DIRECTORY__ + @"/FSharp.Compiler.Service.Tests.fsproj"
-    // Check with Configuration = Release
-    let options = checker.GetProjectOptionsFromProjectFile(projectFile, [("Configuration", "Release")])
+let ``Test Project32 whole project errors`` () = 
 
-    printfn "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv" 
-    printfn "PROJ FILE %s" projectFile
-    printfn "%A" options.OtherOptions
-    printfn "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" 
-    checkOption options.OtherOptions "FSharp.Compiler.Service.dll"
-    checkOption options.OtherOptions "FileSystemTests.fs"
-    checkOption options.OtherOptions "--define:TRACE"
-    checkOptionNotPresent options.OtherOptions "--define:DEBUG"
-    checkOption options.OtherOptions "--debug:pdbonly"
+    let wholeProjectResults = checker.ParseAndCheckProject(Project32.options) |> Async.RunSynchronously
+    wholeProjectResults.Errors.Length |> shouldEqual 0
 
 [<Test>]
-let ``Project file parsing VS2013_FSharp_Portable_Library_net45``() = 
-  // BUG - see https://github.com/fsharp/FSharp.Compiler.Service/issues/237
-  if not runningOnMono then 
-    let projectFile = __SOURCE_DIRECTORY__ + @"/../projects/Sample_VS2013_FSharp_Portable_Library_net45/Sample_VS2013_FSharp_Portable_Library_net45.fsproj"
-    let options = checker.GetProjectOptionsFromProjectFile(projectFile, [])
+let ``Test Project32 should be able to find sig symbols`` () =
 
-    printfn "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv" 
-    printfn "PROJ FILE %s" projectFile
-    printfn "%A" options.OtherOptions
-    printfn "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" 
+    let wholeProjectResults = checker.ParseAndCheckProject(Project32.options) |> Async.RunSynchronously
+    let _sigBackgroundParseResults1, sigBackgroundTypedParse1 = 
+        checker.GetBackgroundCheckResultsForFileInProject(Project32.sigFileName1, Project32.options) 
+        |> Async.RunSynchronously
 
-    checkOption options.OtherOptions "--targetprofile:netcore"
-    checkOption options.OtherOptions "--tailcalls-"
+    let sigSymbolUseOpt = sigBackgroundTypedParse1.GetSymbolUseAtLocation(4,5,"",["func"]) |> Async.RunSynchronously
+    let sigSymbol = sigSymbolUseOpt.Value.Symbol
+    
+    let usesOfSigSymbol = 
+        [ for su in wholeProjectResults.GetUsesOfSymbol(sigSymbol) |> Async.RunSynchronously do
+              yield Project32.cleanFileName su.FileName , tups su.RangeAlternate, attribsOfSymbol su.Symbol ]
 
-    checkOption options.OtherOptions "FSharp.Core.dll"
-    checkOption options.OtherOptions "Microsoft.CSharp.dll"
-    checkOption options.OtherOptions "System.Runtime.dll"
-    checkOption options.OtherOptions "System.Net.Requests.dll"
-    checkOption options.OtherOptions "System.Xml.XmlSerializer.dll"
+    usesOfSigSymbol |> shouldEqual
+       [("sig1", ((4, 4), (4, 8)), ["val"]); 
+        ("file1", ((3, 4), (3, 8)), ["val"])]
 
 [<Test>]
-let ``Project file parsing Sample_VS2013_FSharp_Portable_Library_net451_adjusted_to_profile78``() = 
-  // BUG - see https://github.com/fsharp/FSharp.Compiler.Service/issues/237
-  if not runningOnMono then 
-    let projectFile = __SOURCE_DIRECTORY__ + @"/../projects/Sample_VS2013_FSharp_Portable_Library_net451_adjusted_to_profile78/Sample_VS2013_FSharp_Portable_Library_net451.fsproj"
-    let options = checker.GetProjectOptionsFromProjectFile(projectFile, [])
+let ``Test Project32 should be able to find impl symbols`` () =
 
-    printfn "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv" 
-    printfn "PROJ FILE %s" projectFile
-    printfn "%A" options.OtherOptions
-    printfn "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" 
-    checkOption options.OtherOptions "--targetprofile:netcore"
-    checkOption options.OtherOptions "--tailcalls-"
+    let wholeProjectResults = checker.ParseAndCheckProject(Project32.options) |> Async.RunSynchronously
+    let _implBackgroundParseResults1, implBackgroundTypedParse1 = 
+        checker.GetBackgroundCheckResultsForFileInProject(Project32.fileName1, Project32.options) 
+        |> Async.RunSynchronously
 
-    checkOption options.OtherOptions "FSharp.Core.dll"
-    checkOption options.OtherOptions "Microsoft.CSharp.dll"
-    checkOption options.OtherOptions "System.Runtime.dll"
-    checkOption options.OtherOptions "System.Net.Requests.dll"
-    checkOption options.OtherOptions "System.Xml.XmlSerializer.dll"
+    let implSymbolUseOpt = implBackgroundTypedParse1.GetSymbolUseAtLocation(3,5,"",["func"]) |> Async.RunSynchronously
+    let implSymbol = implSymbolUseOpt.Value.Symbol
+    
+    let usesOfImplSymbol = 
+        [ for su in wholeProjectResults.GetUsesOfSymbol(implSymbol) |> Async.RunSynchronously do
+              yield Project32.cleanFileName su.FileName , tups su.RangeAlternate, attribsOfSymbol su.Symbol ]
 
-[<Test>]
-let ``Project file parsing -- compile files 1``() =
-  let p = FSharpProjectFileInfo.Parse(__SOURCE_DIRECTORY__ + @"/data/Test1.fsproj")
-
-  p.CompileFiles
-  |> List.map Path.GetFileName
-  |> set
-  |> should equal (set [ "Test1File1.fs"; "Test1File2.fs" ])
-
-[<Test>]
-let ``Project file parsing -- compile files 2``() =
-  let p = FSharpProjectFileInfo.Parse(__SOURCE_DIRECTORY__ + @"/data/Test2.fsproj")
-
-  p.CompileFiles
-  |> List.map Path.GetFileName
-  |> set
-  |> should equal (set [ "Test2File1.fs"; "Test2File2.fs" ])
-
-[<Test>]
-let ``Project file parsing -- bad project file``() =
-  (fun () -> FSharpProjectFileInfo.Parse(__SOURCE_DIRECTORY__ + @"/data/Malformed.fsproj") |> ignore)
-  |> should throw typeof<Microsoft.Build.Exceptions.InvalidProjectFileException>
-
-[<Test>]
-let ``Project file parsing -- non-existent project file``() =
-  (fun () -> FSharpProjectFileInfo.Parse(__SOURCE_DIRECTORY__ + @"/data/DoesNotExist.fsproj") |> ignore)
-  |> should throw typeof<System.IO.FileNotFoundException>
-
-[<Test>]
-let ``Project file parsing -- output file``() =
-  let p = FSharpProjectFileInfo.Parse(__SOURCE_DIRECTORY__ + @"/data/Test1.fsproj")
-
-  let expectedOutputPath =
-    (new Uri(__SOURCE_DIRECTORY__ + "/data/Test1/bin/Debug/Test1.dll")).LocalPath
-
-  p.OutputFile
-  |> should equal (Some expectedOutputPath)
-
-[<Test>]
-let ``Project file parsing -- references``() =
-  let p = FSharpProjectFileInfo.Parse(__SOURCE_DIRECTORY__ + @"/data/Test1.fsproj")
-
-  checkOption (Array.ofList p.References) "FSharp.Core.dll"
-  checkOption (Array.ofList p.References) "mscorlib.dll"
-  checkOption (Array.ofList p.References) "System.Core.dll"
-  checkOption (Array.ofList p.References) "System.dll"
-  p.References |> should haveLength 4
-
-[<Test>]
-let ``Project file parsing -- 2nd level references``() =
-  let p = FSharpProjectFileInfo.Parse(__SOURCE_DIRECTORY__ + @"/data/Test2.fsproj")
-
-  checkOption (Array.ofList p.References) "FSharp.Core.dll"
-  checkOption (Array.ofList p.References) "mscorlib.dll"
-  checkOption (Array.ofList p.References) "System.Core.dll"
-  checkOption (Array.ofList p.References) "System.dll"
-  checkOption (Array.ofList p.References) "Test1.dll"
-  p.References |> should haveLength 5
-
-[<Test>]
-let ``Project file parsing -- Tools Version 12``() =
-  let p = FSharpProjectFileInfo.Parse(__SOURCE_DIRECTORY__ + @"/data/ToolsVersion12.fsproj")
-
-  checkOption (Array.ofList p.References) "System.Core.dll"
-
-[<Test>]
-let ``Project file parsing -- Logging``() =
-  let p = FSharpProjectFileInfo.Parse(__SOURCE_DIRECTORY__ + @"/data/ToolsVersion12.fsproj", enableLogging=true)
-
-  if runningOnMono then
-    Assert.That(p.LogOutput, Is.StringContaining("Reference System.Core resolved"))
-    Assert.That(p.LogOutput, Is.StringContaining("Target named 'ImplicitlyExpandTargetFramework'"))
-    Assert.That(p.LogOutput, Is.StringContaining("Using task ResolveAssemblyReference from Microsoft.Build.Tasks.ResolveAssemblyReference, Microsoft.Build.Tasks.v12.0, Version=12.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"))
-  else
-    Assert.That(p.LogOutput, Is.StringContaining("""Using "ResolveAssemblyReference" task from assembly "Microsoft.Build.Tasks.v12.0, Version=12.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"."""))
-    Assert.That(p.LogOutput, Is.StringContaining("""target "ImplicitlyExpandTargetFramework"""))
-
-[<Test>]
-let ``Project file parsing -- Full path``() =
-  let f = (new Uri(__SOURCE_DIRECTORY__ + @"/data/ToolsVersion12.fsproj")).LocalPath
-  let p = FSharpProjectFileInfo.Parse(f)
-
-  p.FullPath |> should equal f
-#endif
+    usesOfImplSymbol |> shouldEqual
+       [("sig1", ((4, 4), (4, 8)), ["val"]); 
+        ("file1", ((3, 4), (3, 8)), ["val"])]
--- tests/service/ProjectOptionsTests.fs
+++ tests/service/ProjectOptionsTests.fs
@@ -10,6 +10,7 @@ module FSharp.Compiler.Service.Tests.ProjectOptionsTests
 let runningOnMono = try System.Type.GetType("Mono.Runtime") <> null with e ->  false
 
 open System
+open System.IO
 open NUnit.Framework
 open FsUnit
 open Microsoft.FSharp.Compiler.SourceCodeServices
@@ -136,6 +137,89 @@ let ``Project file parsing Sample_VS2013_FSharp_Portable_Library_net451_adjusted
     checkOption options.OtherOptions "System.Net.Requests.dll"
     checkOption options.OtherOptions "System.Xml.XmlSerializer.dll"
 
-#endif
+[<Test>]
+let ``Project file parsing -- compile files 1``() =
+  let p = FSharpProjectFileInfo.Parse(__SOURCE_DIRECTORY__ + @"/data/Test1.fsproj")
 
+  p.CompileFiles
+  |> List.map Path.GetFileName
+  |> set
+  |> should equal (set [ "Test1File1.fs"; "Test1File2.fs" ])
+
+[<Test>]
+let ``Project file parsing -- compile files 2``() =
+  let p = FSharpProjectFileInfo.Parse(__SOURCE_DIRECTORY__ + @"/data/Test2.fsproj")
+
+  p.CompileFiles
+  |> List.map Path.GetFileName
+  |> set
+  |> should equal (set [ "Test2File1.fs"; "Test2File2.fs" ])
+
+[<Test>]
+let ``Project file parsing -- bad project file``() =
+  (fun () -> FSharpProjectFileInfo.Parse(__SOURCE_DIRECTORY__ + @"/data/Malformed.fsproj") |> ignore)
+  |> should throw typeof<Microsoft.Build.Exceptions.InvalidProjectFileException>
+
+[<Test>]
+let ``Project file parsing -- non-existent project file``() =
+  (fun () -> FSharpProjectFileInfo.Parse(__SOURCE_DIRECTORY__ + @"/data/DoesNotExist.fsproj") |> ignore)
+  |> should throw typeof<System.IO.FileNotFoundException>
+
+[<Test>]
+let ``Project file parsing -- output file``() =
+  let p = FSharpProjectFileInfo.Parse(__SOURCE_DIRECTORY__ + @"/data/Test1.fsproj")
+
+  let expectedOutputPath =
+    (new Uri(__SOURCE_DIRECTORY__ + "/data/Test1/bin/Debug/Test1.dll")).LocalPath
+
+  p.OutputFile
+  |> should equal (Some expectedOutputPath)
+
+[<Test>]
+let ``Project file parsing -- references``() =
+  let p = FSharpProjectFileInfo.Parse(__SOURCE_DIRECTORY__ + @"/data/Test1.fsproj")
+
+  checkOption (Array.ofList p.References) "FSharp.Core.dll"
+  checkOption (Array.ofList p.References) "mscorlib.dll"
+  checkOption (Array.ofList p.References) "System.Core.dll"
+  checkOption (Array.ofList p.References) "System.dll"
+  p.References |> should haveLength 4
+
+[<Test>]
+let ``Project file parsing -- 2nd level references``() =
+  let p = FSharpProjectFileInfo.Parse(__SOURCE_DIRECTORY__ + @"/data/Test2.fsproj")
+
+  checkOption (Array.ofList p.References) "FSharp.Core.dll"
+  checkOption (Array.ofList p.References) "mscorlib.dll"
+  checkOption (Array.ofList p.References) "System.Core.dll"
+  checkOption (Array.ofList p.References) "System.dll"
+  checkOption (Array.ofList p.References) "Test1.dll"
+  p.References |> should haveLength 5
+
+[<Test>]
+let ``Project file parsing -- Tools Version 12``() =
+  let p = FSharpProjectFileInfo.Parse(__SOURCE_DIRECTORY__ + @"/data/ToolsVersion12.fsproj")
+
+  checkOption (Array.ofList p.References) "System.Core.dll"
+
+[<Test>]
+let ``Project file parsing -- Logging``() =
+  let p = FSharpProjectFileInfo.Parse(__SOURCE_DIRECTORY__ + @"/data/ToolsVersion12.fsproj", enableLogging=true)
+
+  if runningOnMono then
+    Assert.That(p.LogOutput, Is.StringContaining("Reference System.Core resolved"))
+    Assert.That(p.LogOutput, Is.StringContaining("Target named 'ImplicitlyExpandTargetFramework'"))
+    Assert.That(p.LogOutput, Is.StringContaining("Using task ResolveAssemblyReference from Microsoft.Build.Tasks.ResolveAssemblyReference, Microsoft.Build.Tasks.v12.0, Version=12.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"))
+  else
+    Assert.That(p.LogOutput, Is.StringContaining("""Using "ResolveAssemblyReference" task from assembly "Microsoft.Build.Tasks.v12.0, Version=12.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"."""))
+    Assert.That(p.LogOutput, Is.StringContaining("""target "ImplicitlyExpandTargetFramework"""))
+
+[<Test>]
+let ``Project file parsing -- Full path``() =
+  let f = (new Uri(__SOURCE_DIRECTORY__ + @"/data/ToolsVersion12.fsproj")).LocalPath
+  let p = FSharpProjectFileInfo.Parse(f)
+
+  p.FullPath |> should equal f
+
+#endif
 

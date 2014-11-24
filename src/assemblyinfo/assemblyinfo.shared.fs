c3fd8b9db3f94393c329c1f398f96dab5fd9a645
Cameron Taggart <cameron@tachyus.com>
2014-11-24 14:02:51 -0800

    remove generated assemblyinfo from git makes the includes conditional, allows the projects to open when missing

--- src/assemblyinfo/assemblyinfo.shared.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace System
-open System.Reflection
-
-[<assembly: AssemblyVersionAttribute("0.0.80")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.80")>]
-do ()
-
-module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.80"

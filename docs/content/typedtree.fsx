4b6d8db65715d6489830ce469a7e76af2e0dae71
F# Core Engineering Group (Git Repo Management) <fsgit@fsharp.org>
2014-12-08 15:16:56 +0000

    cleaner sample

--- docs/content/typedtree.fsx
+++ docs/content/typedtree.fsx
@@ -38,7 +38,7 @@ One difference is that we set keepAssemblyContents to true.
 let checker = FSharpChecker.Create(keepAssemblyContents=true)
 
 let parseAndCheckSingleFile (input) = 
-    let file = Path.ChangeExtension(System.IO.Path.GetTempFileName(),"fsx")  
+    let file = Path.ChangeExtension(System.IO.Path.GetTempFileName(), "fsx")  
     File.WriteAllText(file, input)
     // Get context representing a stand-alone (script) file
     let projOptions = 
@@ -62,7 +62,7 @@ module MyLibrary
 open System
 
 let foo(x, y) = 
-    let msg = String.Concat("Hello"," ","world")
+    let msg = String.Concat("Hello", " ", "world")
     if msg.Length > 10 then 
         10 
     else 
@@ -97,11 +97,11 @@ In this case there is only one implementation file in the project:
 
 let rec printDecl prefix d = 
     match d with 
-    | FSharpImplementationFileDeclaration.Entity (e,subDecls) -> 
+    | FSharpImplementationFileDeclaration.Entity (e, subDecls) -> 
         printfn "%sEntity %s was declared and contains %d sub-declarations" prefix e.CompiledName subDecls.Length
         for subDecl in subDecls do 
             printDecl (prefix+"    ") subDecl
-    | FSharpImplementationFileDeclaration.MemberOrFunctionOrValue(v,vs,e) -> 
+    | FSharpImplementationFileDeclaration.MemberOrFunctionOrValue(v, vs, e) -> 
         printfn "%sMember or value %s was declared" prefix  v.CompiledName
     | FSharpImplementationFileDeclaration.InitAction(e) -> 
         printfn "%sA top-level expression was declared" prefix 
@@ -129,7 +129,7 @@ contains fours sub-declarations.
 
 let myLibraryEntity, myLibraryDecls =    
    match checkedFile.Declarations.[0] with 
-   | FSharpImplementationFileDeclaration.Entity (e,subDecls) -> (e,subDecls)
+   | FSharpImplementationFileDeclaration.Entity (e, subDecls) -> (e, subDecls)
    | _ -> failwith "unexpected"
 
 
@@ -140,7 +140,7 @@ What about the expressions, for example the body of function "foo"? Let's find i
 
 let (fooSymbol, fooArgs, fooExpression) = 
     match myLibraryDecls.[0] with 
-    | FSharpImplementationFileDeclaration.MemberOrFunctionOrValue(v,vs,e) -> (v,vs,e)
+    | FSharpImplementationFileDeclaration.MemberOrFunctionOrValue(v, vs, e) -> (v, vs, e)
     | _ -> failwith "unexpected"
 
 
@@ -148,7 +148,7 @@ let (fooSymbol, fooArgs, fooExpression) =
 'fooArgs' represents the formal arguments to the 'foo' function, and 'fooExpression' 
 is an expression for the implementation of the 'foo' function.
 
-Once you have an expression, you can work with it much like an F# quotation.  For example,
+Once you have an expression, you can work with it much like an F# quotation.  For example, 
 you can find its declaration range and its type:
 
 *)
@@ -169,52 +169,89 @@ Here is a generic expression visitor:
 let rec visitExpr f (e:FSharpExpr) = 
     f e
     match e with 
-    | BasicPatterns.AddressOf(e1) -> visitExpr f e1
-    | BasicPatterns.AddressSet(e1,e2) -> visitExpr f e1; visitExpr f e2
-    | BasicPatterns.Application(ef,tyargs,args) -> visitExpr f ef; visitExprs f args
-    | BasicPatterns.Call(obj,v,tyargs1,tyargs2,args) -> visitObjArg f obj; visitExprs f args
-    | BasicPatterns.Coerce(ty1,e1) -> visitExpr f e1
-    | BasicPatterns.FastIntegerForLoop(e1,e2,e3,isUp) -> visitExpr f e1; visitExpr f e2; visitExpr f e3
-    | BasicPatterns.ILAsm(s,tyargs,args) -> visitExprs f args
-    | BasicPatterns.ILFieldGet (objOpt, fieldType, fieldName) -> visitObjArg f objOpt
-    | BasicPatterns.ILFieldSet (objOpt, fieldType, fieldName, ve) -> visitObjArg f objOpt
-    | BasicPatterns.IfThenElse (ge,te,ee) -> visitExpr f ge; visitExpr f te; visitExpr f ee
-    | BasicPatterns.Lambda(v,body) -> visitExpr f body
-    | BasicPatterns.Let((v,ve),body) -> visitExpr f ve; visitExpr f body
-    | BasicPatterns.LetRec(vse,body) -> List.iter (snd >> visitExpr f) vse; visitExpr f body
-    | BasicPatterns.NewArray(ty,args) -> visitExprs f args
-    | BasicPatterns.NewDelegate(ty,arg) -> visitExpr f arg
-    | BasicPatterns.NewObject(v,tys,args) -> visitExprs f args
-    | BasicPatterns.NewRecord(v,args) ->  visitExprs f args
-    | BasicPatterns.NewTuple(v,args) -> visitExprs f args
-    | BasicPatterns.NewUnionCase(ty,uc,args) -> visitExprs f args
-    | BasicPatterns.Quote(e1) -> visitExpr f e1
-    | BasicPatterns.FSharpFieldGet(objOpt, ty,fieldInfo) -> visitObjArg f objOpt
-    | BasicPatterns.FSharpFieldSet(objOpt, ty,fieldInfo,arg) -> visitObjArg f objOpt; visitExpr f arg
-    | BasicPatterns.Sequential(e1,e2) -> visitExpr f e1; visitExpr f e2
-    | BasicPatterns.TryFinally(e1,e2) -> visitExpr f e1; visitExpr f e2
-    | BasicPatterns.TryWith(body,_,_,vCatch,eCatch) -> visitExpr f body; visitExpr f eCatch
-    | BasicPatterns.TupleGet(ty,n,e1) -> visitExpr f e1
-    | BasicPatterns.DecisionTree(dtree,targets) -> visitExpr f dtree; List.iter (snd >> visitExpr f) targets
-    | BasicPatterns.DecisionTreeSuccess (tg,es) -> visitExprs f es
-    | BasicPatterns.TypeLambda(gp1,body) -> visitExpr f body
-    | BasicPatterns.TypeTest(ty,e1) -> visitExpr f e1
-    | BasicPatterns.UnionCaseSet(obj,ty,uc,f1,e1) -> visitExpr f obj; visitExpr f e1
-    | BasicPatterns.UnionCaseGet(obj,ty,uc,f1) -> visitExpr f obj
-    | BasicPatterns.UnionCaseTest(obj,ty,f1) -> visitExpr f obj
-    | BasicPatterns.UnionCaseTag(obj,ty) -> visitExpr f obj
-    | BasicPatterns.ObjectExpr(ty,basecall,overrides,iimpls) -> 
-        visitExpr f basecall
+    | BasicPatterns.AddressOf(lvalueExpr) -> 
+        visitExpr f lvalueExpr
+    | BasicPatterns.AddressSet(lvalueExpr, rvalueExpr) -> 
+        visitExpr f lvalueExpr; visitExpr f rvalueExpr
+    | BasicPatterns.Application(funcExpr, typeArgs, argExprs) -> 
+        visitExpr f funcExpr; visitExprs f argExprs
+    | BasicPatterns.Call(objExprOpt, memberOrFunc, typeArgs1, typeArgs2, argExprs) -> 
+        visitObjArg f objExprOpt; visitExprs f argExprs
+    | BasicPatterns.Coerce(targetType, inpExpr) -> 
+        visitExpr f inpExpr
+    | BasicPatterns.FastIntegerForLoop(startExpr, limitExpr, consumeExpr, isUp) -> 
+        visitExpr f startExpr; visitExpr f limitExpr; visitExpr f consumeExpr
+    | BasicPatterns.ILAsm(asmCode, typeArgs, argExprs) -> 
+        visitExprs f argExprs
+    | BasicPatterns.ILFieldGet (objExprOpt, fieldType, fieldName) -> 
+        visitObjArg f objExprOpt
+    | BasicPatterns.ILFieldSet (objExprOpt, fieldType, fieldName, valueExpr) -> 
+        visitObjArg f objExprOpt
+    | BasicPatterns.IfThenElse (guardExpr, thenExpr, elseExpr) -> 
+        visitExpr f guardExpr; visitExpr f thenExpr; visitExpr f elseExpr
+    | BasicPatterns.Lambda(lambdaVar, bodyExpr) -> 
+        visitExpr f bodyExpr
+    | BasicPatterns.Let((bindingVar, bindingExpr), bodyExpr) -> 
+        visitExpr f bindingExpr; visitExpr f bodyExpr
+    | BasicPatterns.LetRec(recursiveBindings, bodyExpr) -> 
+        List.iter (snd >> visitExpr f) recursiveBindings; visitExpr f bodyExpr
+    | BasicPatterns.NewArray(arrayType, argExprs) -> 
+        visitExprs f argExprs
+    | BasicPatterns.NewDelegate(delegateType, delegateBodyExpr) -> 
+        visitExpr f delegateBodyExpr
+    | BasicPatterns.NewObject(objType, typeArgs, argExprs) -> 
+        visitExprs f argExprs
+    | BasicPatterns.NewRecord(recordType, argExprs) ->  
+        visitExprs f argExprs
+    | BasicPatterns.NewTuple(tupleType, argExprs) -> 
+        visitExprs f argExprs
+    | BasicPatterns.NewUnionCase(unionType, unionCase, argExprs) -> 
+        visitExprs f argExprs
+    | BasicPatterns.Quote(quotedExpr) -> 
+        visitExpr f quotedExpr
+    | BasicPatterns.FSharpFieldGet(objExprOpt, recordOrClassType, fieldInfo) -> 
+        visitObjArg f objExprOpt
+    | BasicPatterns.FSharpFieldSet(objExprOpt, recordOrClassType, fieldInfo, argExpr) -> 
+        visitObjArg f objExprOpt; visitExpr f argExpr
+    | BasicPatterns.Sequential(firstExpr, secondExpr) -> 
+        visitExpr f firstExpr; visitExpr f secondExpr
+    | BasicPatterns.TryFinally(bodyExpr, finalizeExpr) -> 
+        visitExpr f bodyExpr; visitExpr f finalizeExpr
+    | BasicPatterns.TryWith(bodyExpr, _, _, catchVar, catchExpr) -> 
+        visitExpr f bodyExpr; visitExpr f catchExpr
+    | BasicPatterns.TupleGet(tupleType, tupleElemIndex, tupleExpr) -> 
+        visitExpr f tupleExpr
+    | BasicPatterns.DecisionTree(decisionExpr, decisionTargets) -> 
+        visitExpr f decisionExpr; List.iter (snd >> visitExpr f) decisionTargets
+    | BasicPatterns.DecisionTreeSuccess (decisionTargetIdx, decisionTargetExprs) -> 
+        visitExprs f decisionTargetExprs
+    | BasicPatterns.TypeLambda(genericParam, bodyExpr) -> 
+        visitExpr f bodyExpr
+    | BasicPatterns.TypeTest(ty, inpExpr) -> 
+        visitExpr f inpExpr
+    | BasicPatterns.UnionCaseSet(unionExpr, unionType, unionCase, unionCaseField, valueExpr) -> 
+        visitExpr f unionExpr; visitExpr f valueExpr
+    | BasicPatterns.UnionCaseGet(unionExpr, unionType, unionCase, unionCaseField) -> 
+        visitExpr f unionExpr
+    | BasicPatterns.UnionCaseTest(unionExpr, unionType, unionCase) -> 
+        visitExpr f unionExpr
+    | BasicPatterns.UnionCaseTag(unionExpr, unionType) -> 
+        visitExpr f unionExpr
+    | BasicPatterns.ObjectExpr(objType, baseCallExpr, overrides, interfaceImplementations) -> 
+        visitExpr f baseCallExpr
         List.iter (visitObjMember f) overrides
-        List.iter (snd >> List.iter (visitObjMember f)) iimpls
-    | BasicPatterns.TraitCall(tys,nm,argtys,tinst,args) -> visitExprs f args
-    | BasicPatterns.ValueSet(v,e1) -> visitExpr f e1
-    | BasicPatterns.WhileLoop(e1,e2) -> visitExpr f e1; visitExpr f e2
-    | BasicPatterns.BaseValue _ -> ()
-    | BasicPatterns.DefaultValue _ -> ()
-    | BasicPatterns.ThisValue _ -> ()
-    | BasicPatterns.Const(obj,ty) -> ()
-    | BasicPatterns.Value(v) -> ()
+        List.iter (snd >> List.iter (visitObjMember f)) interfaceImplementations
+    | BasicPatterns.TraitCall(sourceTypes, traitName, typeArgs, typeInstantiation, argExprs) -> 
+        visitExprs f argExprs
+    | BasicPatterns.ValueSet(valToSet, valueExpr) -> 
+        visitExpr f valueExpr
+    | BasicPatterns.WhileLoop(guardExpr, bodyExpr) -> 
+        visitExpr f guardExpr; visitExpr f bodyExpr
+    | BasicPatterns.BaseValue baseType -> ()
+    | BasicPatterns.DefaultValue defaultType -> ()
+    | BasicPatterns.ThisValue thisType -> ()
+    | BasicPatterns.Const(constValueObj, constType) -> ()
+    | BasicPatterns.Value(valueToGet) -> ()
     | _ -> failwith (sprintf "unrecognized %+A" e)
 
 and visitExprs f exprs = 
@@ -236,9 +273,9 @@ fooExpression |> visitExpr (fun e -> printfn "Visiting %A" e)
 //
 // Visiting Let...
 // Visiting Call...
-// Visiting Const ("Hello",...)
-// Visiting Const (" ",...)
-// Visiting Const ("world",...)
+// Visiting Const ("Hello", ...)
+// Visiting Const (" ", ...)
+// Visiting Const ("world", ...)
 // Visiting IfThenElse...
 // Visiting Call...
 // Visiting Call...

236037e668c3a1a2517b3e837eef5d7654b1a31c
Don Syme <donsyme@fastmail.fm>
2015-10-16 13:57:10 +0100

    use trace not debug

--- src/fsharp/vs/Reactor.fs
+++ src/fsharp/vs/Reactor.fs
@@ -44,7 +44,7 @@ module internal Reactor =
                                              
             // Async workflow which receives messages and dispatches to worker functions.
             let rec loop (bgOpOpt, onComplete, bg) = 
-                async { Debug.WriteLine("Reactor: receiving..., remaining {0}, mem {1}, gc2 {2}", inbox.CurrentQueueLength, GC.GetTotalMemory(false)/1000000L, GC.CollectionCount(2))
+                async { Trace.TraceInformation("Reactor: receiving..., remaining {0}, mem {1}, gc2 {2}", inbox.CurrentQueueLength, GC.GetTotalMemory(false)/1000000L, GC.CollectionCount(2))
                         
                         // Messages always have priority over the background op.
                         let! msg = 
@@ -61,37 +61,37 @@ module internal Reactor =
 
                         match msg with
                         | Some (SetBackgroundOp bgOpOpt) -> 
-                            Debug.WriteLine("Reactor: --> set background op, remaining {0}, mem {1}, gc2 {2}", inbox.CurrentQueueLength, GC.GetTotalMemory(false)/1000000L, GC.CollectionCount(2))
+                            Trace.TraceInformation("Reactor: --> set background op, remaining {0}, mem {1}, gc2 {2}", inbox.CurrentQueueLength, GC.GetTotalMemory(false)/1000000L, GC.CollectionCount(2))
                             return! loop (bgOpOpt, onComplete, false)
                         | Some (Op (desc, ct, op, ccont)) -> 
                             if ct.IsCancellationRequested then ccont() else
-                            Debug.WriteLine("Reactor: --> {0}, remaining {1}, mem {2}, gc2 {3}", desc, inbox.CurrentQueueLength, GC.GetTotalMemory(false)/1000000L, GC.CollectionCount(2))
+                            Trace.TraceInformation("Reactor: --> {0}, remaining {1}, mem {2}, gc2 {3}", desc, inbox.CurrentQueueLength, GC.GetTotalMemory(false)/1000000L, GC.CollectionCount(2))
                             let time = System.DateTime.Now
                             op()
                             let span = System.DateTime.Now - time
                             //if span.TotalMilliseconds > 100.0 then 
-                            Debug.WriteLine("Reactor: <-- {0}, remaining {1}, took {2}ms", desc, inbox.CurrentQueueLength, span.TotalMilliseconds)
+                            Trace.TraceInformation("Reactor: <-- {0}, remaining {1}, took {2}ms", desc, inbox.CurrentQueueLength, span.TotalMilliseconds)
                             return! loop (bgOpOpt, onComplete, false)
                         | Some (WaitForBackgroundOpCompletion channel) -> 
-                            Debug.WriteLine("Reactor: --> wait for background (debug only), remaining {0}, mem {1}, gc2 {2}", inbox.CurrentQueueLength, GC.GetTotalMemory(false)/1000000L, GC.CollectionCount(2))
+                            Trace.TraceInformation("Reactor: --> wait for background (debug only), remaining {0}, mem {1}, gc2 {2}", inbox.CurrentQueueLength, GC.GetTotalMemory(false)/1000000L, GC.CollectionCount(2))
                             match bgOpOpt with 
                             | None -> ()
                             | Some bgOp -> while bgOp() do ()
                             channel.Reply(())
                             return! loop (None, onComplete, false)
                         | Some (CompleteAllQueuedOps channel) -> 
-                            Debug.WriteLine("Reactor: --> stop background work and complete all queued ops, remaining {0}, mem {1}, gc2 {2}", inbox.CurrentQueueLength, GC.GetTotalMemory(false)/1000000L, GC.CollectionCount(2))
+                            Trace.TraceInformation("Reactor: --> stop background work and complete all queued ops, remaining {0}, mem {1}, gc2 {2}", inbox.CurrentQueueLength, GC.GetTotalMemory(false)/1000000L, GC.CollectionCount(2))
                             return! loop (None, Some channel, false)
                         | None -> 
                             match bgOpOpt, onComplete with 
                             | _, Some onComplete -> onComplete.Reply()
                             | Some bgOp, None -> 
-                                Debug.WriteLine("Reactor: --> background step, remaining {0}, mem {1}, gc2 {2}", inbox.CurrentQueueLength, GC.GetTotalMemory(false)/1000000L, GC.CollectionCount(2))
+                                Trace.TraceInformation("Reactor: --> background step, remaining {0}, mem {1}, gc2 {2}", inbox.CurrentQueueLength, GC.GetTotalMemory(false)/1000000L, GC.CollectionCount(2))
                                 let time = System.DateTime.Now
                                 let res = bgOp()
                                 let span = System.DateTime.Now - time
                                 //if span.TotalMilliseconds > 100.0 then 
-                                Debug.WriteLine("Reactor: <-- background step, remaining {0}, took {1}ms", inbox.CurrentQueueLength, span.TotalMilliseconds)
+                                Trace.TraceInformation("Reactor: <-- background step, remaining {0}, took {1}ms", inbox.CurrentQueueLength, span.TotalMilliseconds)
                                 return! loop ((if res then Some bgOp else None), onComplete, true)
                             | None, None -> failwith "unreachable, should have used inbox.Receive"
                       }
@@ -106,15 +106,15 @@ module internal Reactor =
 
         // [Foreground Mailbox Accessors] -----------------------------------------------------------                
         member r.SetBackgroundOp(build) = 
-            Debug.WriteLine("Reactor: enqueue start background, length {0}", builder.CurrentQueueLength)
+            Trace.TraceInformation("Reactor: enqueue start background, length {0}", builder.CurrentQueueLength)
             builder.Post(SetBackgroundOp build)
 
         member r.EnqueueOp(desc, op) =
-            Debug.WriteLine("Reactor: enqueue {0}, length {1}", desc, builder.CurrentQueueLength)
+            Trace.TraceInformation("Reactor: enqueue {0}, length {1}", desc, builder.CurrentQueueLength)
             builder.Post(Op(desc, CancellationToken.None, op, (fun () -> ()))) 
 
         member r.EnqueueOpPrim(desc, ct, op, ccont) =
-            Debug.WriteLine("Reactor: enqueue {0}, length {1}", desc, builder.CurrentQueueLength)
+            Trace.TraceInformation("Reactor: enqueue {0}, length {1}", desc, builder.CurrentQueueLength)
             builder.Post(Op(desc, ct, op, ccont)) 
 
         member r.CurrentQueueLength =
@@ -122,12 +122,12 @@ module internal Reactor =
 
         // This is for testing only
         member r.WaitForBackgroundOpCompletion() =
-            Debug.WriteLine("Reactor: enqueue wait for background, length {0}", builder.CurrentQueueLength)
+            Trace.TraceInformation("Reactor: enqueue wait for background, length {0}", builder.CurrentQueueLength)
             builder.PostAndReply WaitForBackgroundOpCompletion 
 
         // This is for testing only
         member r.CompleteAllQueuedOps() =
-            Debug.WriteLine("Reactor: enqueue wait for background, length {0}", builder.CurrentQueueLength)
+            Trace.TraceInformation("Reactor: enqueue wait for background, length {0}", builder.CurrentQueueLength)
             builder.PostAndReply WaitForBackgroundOpCompletion 
 
         member r.EnqueueAndAwaitOpAsync (desc, f) = 

4bc19b268e29c8ad2031c70060fa8c46f293afb2
7sharp9 <dave.thomas@xamarin.com>
2016-01-18 18:08:07 +0000

    Update psi to capture providedTypes from the static linker
    These are then passed into GenerateIlxCode.

--- src/fsharp/fsi/fsi.fs
+++ src/fsharp/fsi/fsi.fs
@@ -912,9 +912,10 @@ type internal FsiDynamicCompiler
         let optimizedImpls, _optData, optEnv = ApplyAllOptimizations (tcConfig, tcGlobals, (LightweightTcValForUsingInBuildMethodCall tcGlobals), outfile, importMap, isIncrementalFragment, optEnv, tcState.Ccu, declaredImpls)
         errorLogger.AbortOnError(fsiConsoleOutput);
             
-        let fragName = textOfLid prefixPath 
-        let codegenResults = GenerateIlxCode (IlReflectBackend, isInteractiveItExpr, runningOnMono, tcConfig, topCustomAttrs, optimizedImpls, fragName, true, ilxGenerator, [])
-        errorLogger.AbortOnError();
+        let fragName = textOfLid prefixPath
+        let providedTypes, staticLinker = Driver.StaticLinker.StaticLink(tcConfig, tcImports, ilGlobals)
+        let codegenResults = GenerateIlxCode (IlReflectBackend, isInteractiveItExpr, runningOnMono, tcConfig, topCustomAttrs, optimizedImpls, fragName, true, ilxGenerator, providedTypes)
+        errorLogger.AbortOnError(fsiConsoleOutput);
 
         // Each input is like a small separately compiled extension to a single source file. 
         // The incremental extension to the environment is dictated by the "signature" of the values as they come out 

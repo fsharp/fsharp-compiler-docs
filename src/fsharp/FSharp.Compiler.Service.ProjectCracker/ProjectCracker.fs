0462bc32afdee50be5597ccdd3c38f3ac2dab2ab
Robin Neatherway <robin.neatherway@gmail.com>
2016-02-04 11:21:55 +0000

    Project cracking: spaces in file paths

--- src/fsharp/FSharp.Compiler.Service.ProjectCracker/ProjectCracker.fs
+++ src/fsharp/FSharp.Compiler.Service.ProjectCracker/ProjectCracker.fs
@@ -28,7 +28,7 @@ type ProjectCracker =
               UnresolvedReferences = None }
 
         let arguments = new StringBuilder()
-        arguments.Append(projectFileName) |> ignore
+        arguments.Append('"').Append(projectFileName).Append('"') |> ignore
         arguments.Append(' ').Append(enableLogging.ToString()) |> ignore
         for k, v in properties do
             arguments.Append(' ').Append(k).Append(' ').Append(v) |> ignore

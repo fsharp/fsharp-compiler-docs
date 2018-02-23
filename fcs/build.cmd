93b76c2f7ad31752012ef1e1b4f012b9ade12a88
Don Syme <dsyme@microsoft.com>
2018-02-23 02:39:06 +0000

    undo workaround for failing paket bootstrapper

--- fcs/build.cmd
+++ fcs/build.cmd
@@ -9,8 +9,8 @@ if errorlevel 1 (
 )
 
 REM see https://github.com/fsprojects/Paket/issues/3065#issuecomment-367829309
-powershell -Command {new-itemproperty -path "HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319" -name "SchUseStrongCrypto" -Value 1 -PropertyType "DWord";}
-powershell -Command {new-itemproperty -path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319" -name "SchUseStrongCrypto" -Value 1 -PropertyType "DWord"}
+REM powershell -Command {new-itemproperty -path "HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319" -name "SchUseStrongCrypto" -Value 1 -PropertyType "DWord";}
+REM powershell -Command {new-itemproperty -path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319" -name "SchUseStrongCrypto" -Value 1 -PropertyType "DWord"}
 
 .paket\paket.exe restore
 if errorlevel 1 (

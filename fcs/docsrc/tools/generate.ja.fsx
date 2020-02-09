f4e36be34dc14c879c548b83f95ed8b34eb5cc0c
nojaf <florian.verdonck@outlook.com>
2020-02-09 21:34:29 +0100

    Added Japanese documentation.

--- fcs/docsrc/tools/generate.ja.fsx
+++ fcs/docsrc/tools/generate.ja.fsx
@@ -3,6 +3,12 @@
 // (the generated documentation is stored in the 'docs' directory)
 // --------------------------------------------------------------------------------------
 
+#r "paket: groupref generate //"
+#load "./.fake/generate.ja.fsx/intellisense.fsx"
+
+// Binaries that have XML documentation (in a corresponding generated XML file)
+let referenceBinaries = [ "../../../artifacts/bin/fcs/Release/net461/FSharp.Compiler.Service.dll" ]
+
 // Web site location for the generated documentation
 let website = "/FSharp.Compiler.Service/ja"
 
@@ -18,35 +24,22 @@ let info =
 // For typical project, no changes are needed below
 // --------------------------------------------------------------------------------------
 
-#I "../../packages/FSharpVSPowerTools.Core/lib/net461"
-#I "../../packages/FSharp.Formatting/lib/net40"
-#I "../../packages/FSharp.Compiler.Service/lib/net461"
-#I "../../packages/FAKE/tools"
-#r "FSharpVSPowerTools.Core.dll"
-#r "System.Web.Razor.dll"
-#r "FakeLib.dll"
-#r "FSharp.Compiler.Service.dll"
-#r "RazorEngine.dll"
-#r "FSharp.Literate.dll"
-#r "FSharp.CodeFormat.dll"
-#r "FSharp.MetadataFormat.dll"
-open Fake
-open System.IO
-open Fake.FileHelper
-open FSharp.Literate
-open FSharp.MetadataFormat
+open Fake.IO.FileSystemOperators
+open Fake.IO
+open Fake.Core
+open FSharp.Formatting.Razor
 
 // When called from 'build.fsx', use the public project URL as <root>
 // otherwise, use the current 'output' directory.
 let root = "."
 
 // Paths with template/source/output locations
-let bin         = __SOURCE_DIRECTORY__ @@ "../../../release/fcs/net461"
+let bin         = __SOURCE_DIRECTORY__ @@ "../../../release/fcs/netcoreapp3.0"
 let content     = __SOURCE_DIRECTORY__ @@ "../content/ja"
 let outputJa    = __SOURCE_DIRECTORY__ @@ "../../../docs/ja"
 let files       = __SOURCE_DIRECTORY__ @@ "../files"
 let templates   = __SOURCE_DIRECTORY__ @@ "templates/ja"
-let formatting  = __SOURCE_DIRECTORY__ @@ "../../packages/FSharp.Formatting/"
+let formatting  = __SOURCE_DIRECTORY__ @@ "../../packages/generate/FSharp.Formatting"
 let docTemplate = formatting @@ "templates/docpage.cshtml"
 
 // Where to look for *.csproj templates (in this order)
@@ -60,10 +53,9 @@ let layoutRoots =
 let buildDocumentation () =
   for dir in [content] do
     let sub = if dir.Length > content.Length then dir.Substring(content.Length + 1) else "."
-    printfn "root = %s" root
-    Literate.ProcessDirectory
+    RazorLiterate.ProcessDirectory
       ( dir, docTemplate, outputJa @@ sub, replacements = ("root", root)::info,
-        layoutRoots = layoutRoots, generateAnchors = true )
+        layoutRoots = layoutRoots, generateAnchors = true, processRecursive=false )
 
 // Generate
 buildDocumentation()

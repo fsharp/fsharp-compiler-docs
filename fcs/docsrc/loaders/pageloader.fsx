7d6eb3e5018355df035ec6dbd4d0d260f8b4eb8c
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2020-06-09 15:13:39 +0200

    Add more links to the shortcuts

--- fcs/docsrc/loaders/pageloader.fsx
+++ fcs/docsrc/loaders/pageloader.fsx
@@ -9,5 +9,8 @@ type Shortcut = {
 
 let loader (projectRoot: string) (siteContet: SiteContents) =
     siteContet.Add({title = "Home"; link = "/"; icon = "fas fa-home"})
-    siteContet.Add({title = "GitHub repo"; link = "TODO: ADD_LINK"; icon = "fab fa-github"})
+    siteContet.Add({title = "F# Software Foundation"; link = "https://fsharp.org"; icon = "fas fa-globe"})
+    siteContet.Add({title = "GitHub repo"; link = "https://github.com/fsharp/FSharp.Compiler.Service"; icon = "fab fa-github"})
+    siteContet.Add({title = "License"; link = "https://github.com/fsharp/FSharp.Compiler.Service/blob/master/LICENSE"; icon = "far fa-file"})
+    siteContet.Add({title = "Release Notes"; link = "https://github.com/fsharp/FSharp.Compiler.Service/blob/master/RELEASE_NOTES.md"; icon = "far fa-file-alt"})
     siteContet

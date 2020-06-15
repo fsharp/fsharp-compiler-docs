975e10c91322c6f1efb4d3786dee9a7ab8ef18c5
Brett V. Forsgren <brettfo@microsoft.com>
2020-06-15 13:24:21 -0700

    migrate to netcoreapp3.1 (#9448)
    * migrate to netcoreapp3.1
    
    * update comments
    
    * update tool versions

--- tests/FSharp.Test.Utilities/TestFramework.fs
+++ tests/FSharp.Test.Utilities/TestFramework.fs
@@ -202,10 +202,10 @@ let config configurationName envVars =
     let fsharpCompilerInteractiveSettingsArchitecture = "net472"
     let peverifyArchitecture = "net472"
 #else
-    let fscArchitecture = "netcoreapp3.0"
-    let fsiArchitecture = "netcoreapp3.0"
+    let fscArchitecture = "netcoreapp3.1"
+    let fsiArchitecture = "netcoreapp3.1"
     let fsharpCoreArchitecture = "netstandard2.0"
-    let fsharpBuildArchitecture = "netcoreapp3.0"
+    let fsharpBuildArchitecture = "netcoreapp3.1"
     let fsharpCompilerInteractiveSettingsArchitecture = "netstandard2.0"
     let peverifyArchitecture = "netcoreapp3.0"
 #endif

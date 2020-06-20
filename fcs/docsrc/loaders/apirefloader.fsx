6d583695ba6204e86e1f4328f289625f9d6b7ca3
Chet Husk <chusk3@gmail.com>
2020-06-20 12:48:27 -0500

    fix docs generation and fail-fast if they cannot be made

--- fcs/docsrc/loaders/apirefloader.fsx
+++ fcs/docsrc/loaders/apirefloader.fsx
@@ -28,43 +28,41 @@ let rec collectModules pn pu nn nu (m: Module) =
 
 
 let loader (projectRoot: string) (siteContent: SiteContents) =
-    try
-      let label = "FSharp.Compiler.Service"
-      let dll = Path.Combine(projectRoot, "..", "..", "artifacts", "bin", "fcs", "Release", "netcoreapp3.0", "FSharp.Compiler.Service.dll")
-      let libs = Path.Combine(projectRoot, "..", "..", "artifacts", "bin", "fcs", "Release", "netcoreapp3.0")
+    let label = "FSharp.Compiler.Service"
+    let dll = Path.Combine(projectRoot, "..", "..", "artifacts", "bin", "fcs", "Release", "netstandard2.0", "FSharp.Compiler.Service.dll")
+    let libs = Path.Combine(projectRoot, "..", "..", "artifacts", "bin", "fcs", "Release", "netstandard2.0")
 
-      printfn "generating api ref for %s at %s" label dll
+    if (not (File.Exists dll)) then failwithf "Unable to find FCS dll at %s" dll
 
-      let output = MetadataFormat.Generate(dll, markDownComments = true, publicOnly = true, libDirs = [libs])
+    printfn "generating api ref for %s at %s" label dll
 
-      let allModules =
-          output.AssemblyGroup.Namespaces
-          |> List.collect (fun n ->
-              List.collect (collectModules n.Name n.Name n.Name n.Name) n.Modules
-          )
+    let output = MetadataFormat.Generate(dll, markDownComments = true, publicOnly = true, libDirs = [libs])
 
-      let allTypes =
-          [
-              yield!
-                  output.AssemblyGroup.Namespaces
-                  |> List.collect (fun n ->
-                      n.Types |> List.map (fun t -> {ParentName = n.Name; ParentUrlName = n.Name; NamespaceName = n.Name; NamespaceUrlName = n.Name; Info = t} )
-                  )
-              yield!
-                  allModules
-                  |> List.collect (fun n ->
-                      n.Info.NestedTypes |> List.map (fun t -> {ParentName = n.Info.Name; ParentUrlName = n.Info.UrlName; NamespaceName = n.NamespaceName; NamespaceUrlName = n.NamespaceUrlName; Info = t}) )
-          ]
-      let entities = {
-        Label = label
-        Modules = allModules
-        Types = allTypes
-        GeneratorOutput = output
-      }
-      siteContent.Add entities
-      printfn "generated api ref for %s at %s" label dll
-    with
-    | ex ->
-      printfn "%A" ex
+    let allModules =
+      output.AssemblyGroup.Namespaces
+      |> List.collect (fun n ->
+          List.collect (collectModules n.Name n.Name n.Name n.Name) n.Modules
+      )
+
+    let allTypes =
+      [
+          yield!
+              output.AssemblyGroup.Namespaces
+              |> List.collect (fun n ->
+                  n.Types |> List.map (fun t -> {ParentName = n.Name; ParentUrlName = n.Name; NamespaceName = n.Name; NamespaceUrlName = n.Name; Info = t} )
+              )
+          yield!
+              allModules
+              |> List.collect (fun n ->
+                  n.Info.NestedTypes |> List.map (fun t -> {ParentName = n.Info.Name; ParentUrlName = n.Info.UrlName; NamespaceName = n.NamespaceName; NamespaceUrlName = n.NamespaceUrlName; Info = t}) )
+      ]
+    let entities = {
+    Label = label
+    Modules = allModules
+    Types = allTypes
+    GeneratorOutput = output
+    }
+    siteContent.Add entities
+    printfn "generated api ref for %s at %s" label dll
 
     siteContent
